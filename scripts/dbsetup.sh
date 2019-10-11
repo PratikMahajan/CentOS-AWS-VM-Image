f36edc4946facb7ff0ac5837a6478f5f9ed50c07
pratikmahajan <pratik@mahajan.xyz>
2019-10-11 15:29:17 -0400

    fixed all the errors with the ami dbsetup installation for centos

--- scripts/dbsetup.sh
+++ scripts/dbsetup.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
-
 if [  -n "$(uname -a | grep Ubuntu)" ]; then
     # OS is Ubuntu
-    apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-    add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.3/ubuntu bionic main'
-    apt update
-    apt install mariadb-server -y
+    sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+    sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.3/ubuntu bionic main'
+    sudo apt update
+    sudo apt install mariadb-server -y
 
 
 else
@@ -25,17 +20,19 @@ gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
 )
-    echo "$REPO" > "$DEST_DIR"
+    echo "$REPO" > MariaDB.repo
+    sudo mv MariaDB.repo "$DEST_DIR"
+#    sudo echo "$REPO" > "$DEST_DIR"
 
-    yum clean all
-    rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-    yum install MariaDB-server -y
+    sudo yum clean all
+    sudo rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+    sudo yum install MariaDB-server -y
 
 
 fi
 
-systemctl enable mariadb
-systemctl start mariadb
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
 
-mysql_secure_installation
+#sudo mysql_secure_installation
 #systemctl status mariadb
